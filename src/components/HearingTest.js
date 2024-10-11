7d1ee25b1b7ea66918ca8b82e50b67c153efa8c3
BenyThomas <banyenzachi@gmail.com>
2024-10-11 19:40:20 +0300

    Added error

--- src/components/HearingTest.js
+++ src/components/HearingTest.js
@@ -88,7 +88,7 @@ const HearingTest = () => {
               </>
             )}
 
-            {testStarted && !isComplete && (
+            {testStarted && !isComplete &&we (
               <>
                 {/* Progress Bar */}
                 <div className="relative w-full h-4 bg-gray-200 rounded-lg overflow-hidden mb-8 animate-fadeIn hover-section">
